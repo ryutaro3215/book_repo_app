fdd006495df84408547f7916829b1f7438b82c2d
rmatsuba <87314627+ryutaro3215@users.noreply.github.com>
2025-08-29 16:56:45 +0900

    make password reset page

---
+++ frontend/src/pages/ResetPasswordPage.tsx
@@ -0,0 +1,9 @@
+import ResetPasswordForm from "../components/ResetPasswordForm";
+
+export default function ResetPasswordPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <ResetPasswordForm />
+    </div>
+  );
+}
